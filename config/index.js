7c663c93ca59d59d9bf3502cb6357098d98fc3f0
yifeitai <neversaynever1022@gmail.com>
2022-12-12 18:06:08 -0500

    add heroku database url

--- config/index.js
+++ config/index.js
@@ -1,16 +1,17 @@
-require("dotenv").config();
-const { Pool } = require("pg");
-const fs = require('fs')
-const path = require('path')
+require('dotenv').config();
+const { Pool } = require('pg');
+const fs = require('fs');
+const path = require('path');
 
-
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === 'production';
 const database =
-  process.env.NODE_ENV === "test"
+  process.env.NODE_ENV === 'test'
     ? process.env.PGDATABASE_TEST
     : process.env.PGDATABASE;
 
-const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${database}`;
+// const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${database}`;
+const connectionString = `postgres://aneqosrshbxbhl:dd20cf3b8639ad7b062c0eb4424259cfa7ad7e981b728324a2c043f108358c9e@ec2-44-194-92-192.compute-1.amazonaws.com:5432/darnjs34g4majb`;
+
 const pool = new Pool({
   connectionString: connectionString,
   /*
@@ -18,7 +19,7 @@ const pool = new Pool({
     Alternatively, you can omit the ssl configuration object if you specify the PGSSLMODE config var: heroku config:set PGSSLMODE=no-verify
     See https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-node-js
     */
-  ssl: false
+  ssl: false,
 });
 
 // run this to create schema
@@ -29,7 +30,6 @@ const pool = new Pool({
 //   }
 // })
 
-
 // Run this for inserting data
 // var file = fs.readFileSync(path.join(__dirname, "data.sql")).toString()
 // pool.query(file, function (err) {
@@ -37,7 +37,6 @@ const pool = new Pool({
 //     console.log(err)
 //   }
 // })
-
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
